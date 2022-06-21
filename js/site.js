1db16e45946f3387a3540ddc5b68ca164659c6f6
mateomaza <1mateomaza@gmail.com>
2022-06-20 22:53:14 -0500

    finished the js
    i made it so it displays in one way if it's a palindrome, and in another if it's not

--- js/site.js
+++ js/site.js
@@ -1,28 +1,62 @@
+// We call the HTML elements we need
+
 const btn = document.getElementById("button")
+const para = document.getElementById("p")
+const style = document.getElementById("alert")
+
+
+// Controller
 
 function getString() {
-    let txt = document.getElementById("txt").value
-    let validString = toString(txt)
-    if (typeof validString == String) {
-        let reversed_array = reverseArray(string)
-        displayReversed(reversed_array)
+
+    let str = document.getElementById("string").value;
+
+    if (str.length > 1) {
+
+        let reversedString = reverseLoop(str);
+
+        displayReversed(reversedString, str);
     } else {
         alert("Input must be a string of 2 or more characters")
     }
 }
 
-// ex: "hello world"
-function reverseArray() {
-    let new_array = []
-    for (let i = string.lenght - 1; i >= 0; i--) {
-        new_array += string[i]
 
+// Logic 
+
+function reverseLoop(string) {
+
+    let newString = "";
+
+    for (let index = string.length - 1; index >= 0; index--) {
+
+        newString += string[index]
     }
 
+    return newString;
 }
 
-function displayReversed() {
 
+//Display
+
+function displayReversed(reversed, string) {
+
+    
+    if (string == reversed) {
+        para.innerHTML = []
+        para.innerHTML = `This is a palindrome! -> ${reversed}<br>(at least if you actually typed words...)`;
+        style.classList.remove("alert-primary")
+        style.classList.add("alert-info");
+
+    } else {
+        para.innerHTML = []
+        para.innerHTML = `Your reversed string is: ${reversed}`;
+        style.classList.remove("alert-info")
+        style.classList.add("alert-primary");
+    }
+
+    style.classList.remove("invisible");
 }
+
 
 btn.addEventListener("click", getString)
