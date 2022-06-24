e10b037991581d62402eb4233a2f40817b64ed07
mateomaza <1mateomaza@gmail.com>
2022-06-24 18:24:48 -0500

    fixed a bug
    i'll finish the layout when i create the mini-site template

--- js/site.js
+++ js/site.js
@@ -1,7 +1,8 @@
 // We call the HTML elements we need
 
 const btn = document.getElementById("button")
-const para = document.getElementById("p")
+const para1 = document.getElementById("p1")
+const head2 = document.getElementById("p2")
 const style = document.getElementById("alert")
 
 
@@ -15,7 +16,7 @@ function getString() {
 
         let reversedString = reverseLoop(str);
 
-        displayReversed(reversedString, str);
+        displayReversed(str, reversedString);
     } else {
         alert("Input must be a string of 2 or more characters")
     }
@@ -39,19 +40,29 @@ function reverseLoop(string) {
 
 //Display
 
-function displayReversed(reversed, string) {
+function displayReversed(string, reversed) {
+
+    let regex = /[^a-z0-9]/gi;
+
+    let reversedFiltered = reversed.toLowerCase();
+    reversedFiltered = reversedFiltered.replace(regex, "");
+
+    let stringFiltered = string.toLowerCase();
+    stringFiltered = stringFiltered.replace(regex, "");
+
 
     
-    if (string == reversed) {
-        para.innerHTML = []
-        para.innerHTML = `This is a palindrome! -> ${reversed}<br>(at least if you actually typed words...)`;
-        style.classList.remove("alert-primary")
+    if (stringFiltered == reversedFiltered) {
+        p1.innerHTML = `This is a palindrome! <br><span class="fs-5 fw-normal">
+        (at least if you actually typed words...)</span>`;
+        p2.innerHTML = `Your reverse string is: ${reversed}`;
+        style.classList.remove("alert-primary");
         style.classList.add("alert-info");
 
     } else {
-        para.innerHTML = []
-        para.innerHTML = `Your reversed string is: ${reversed}`;
-        style.classList.remove("alert-info")
+        p1.innerHTML = `This is is not a palindrome`;
+        p2.innerHTML = `Your reversed string is: ${reversed}`;
+        style.classList.remove("alert-info");
         style.classList.add("alert-primary");
     }
 
